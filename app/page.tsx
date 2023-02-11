70238457605d7aba23522516dfe4c065773764c3
nathan <thenathannicholson@gmail.com>
2023-02-11 14:04:04 -0800

    Changing color using the mouse

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,65 @@ import { Inter } from "@next/font/google"
 import styles from "./page.module.css"
 import { useRef, useState } from "react"
 import { Canvas, useFrame, ThreeElements } from "@react-three/fiber"
+import {
+  OrthographicCamera,
+  PerformanceMonitor,
+  Stats,
+} from "@react-three/drei"
+import { Perf } from "r3f-perf"
 
 const inter = Inter({ subsets: ["latin"] })
 
-function Box(props: ThreeElements["mesh"]) {
+function Plane(props: ThreeElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!)
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
+
+  const uniforms = {
+    u_time: { value: 0.0 },
+    u_mouse: { value: { x: 0.0, y: 0.0 } },
+    u_resolution: { value: { x: 0.0, y: 0.0 } },
+    u_color: { value: new THREE.Color(0x00ff00) },
+  }
+
+  const vshader = `
+  void main(){
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 0.5, 1.0);
+  }
+  `
+
+  const fshader = `
+  uniform vec2 u_mouse;
+  uniform vec2 u_resolution;
+  uniform vec3 u_color;
+  
+  void main(){
+    vec3 color = vec3(u_mouse.x/u_resolution.x, 0.0, u_mouse.y/u_resolution.y);
+    gl_FragColor = vec4(color, 1.0);
+  }
+  `
+  useFrame((state, delta) => {
+    uniforms.u_mouse.value.x = Math.abs(state.mouse.x * 10000)
+    uniforms.u_mouse.value.y = Math.abs(state.mouse.y * 10000)
+    uniforms.u_time.value += delta
+    if (uniforms.u_resolution !== undefined) {
+      uniforms.u_resolution.value.x = state.size.width
+      uniforms.u_resolution.value.y = state.size.height
+    }
+    console.log(
+      uniforms.u_mouse.value.x / uniforms.u_resolution.value.x,
+      uniforms.u_mouse.value.y / uniforms.u_resolution.value.y
+    )
+  })
+
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    <mesh {...props} ref={ref}>
+      <planeGeometry args={[4, 4]} />
+      <shaderMaterial
+        uniforms={uniforms}
+        vertexShader={vshader}
+        fragmentShader={fshader}
+      />
     </mesh>
   )
 }
@@ -31,9 +71,11 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[0, 0, 0]} />
+        <Perf />
+
+        <OrthographicCamera args={[-1, 1, 1, -1, 0.1, 10]}>
+          <Plane />
+        </OrthographicCamera>
       </Canvas>
     </main>
   )
